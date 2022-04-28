64a92ee74840ff6326973f48c0d4c405ca65d5a5
SimoneCorona <corona.simone93@gmail.com>
2022-04-28 15:05:19 +0200

    Scaffholding

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Viene fornito un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare le relative informazioni: Nome, Ruolo e Foto.
+// MILESTONE 1:
+// stampare su console le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// stampare le stesse informazioni su DOM sottoforma di stringhe
